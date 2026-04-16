5780294345eec847824eab55e3f093dad4b3c8db
RaceproxateDev <coxemeftigtipout@gmail.com>
2026-04-16 16:42:32 +0200

    Update Destruction.js

--- Shard Incremental/gameScripts/Destruction.js
+++ Shard Incremental/gameScripts/Destruction.js
@@ -1,9 +1,11 @@
-
+let btn = document.getElementById("DestructionResetBtn");
+let reqTxt = document
 
 function DestructionReset(force) {
     if (Data.constructionPoints.gte(Data.DestructionReq)) {
         if (!force) {
             Data.Destructions = Data.Destructions.add(1);
+            Data.DestructionReq = Data.DestructionReq.mul(Data.DestructionScale);
         }
 
         resetStats(5, 0)
@@ -16,5 +18,4 @@ function DestructionReset(force) {
 }
 
 function updateHtmlDestruction() {
-
 }
