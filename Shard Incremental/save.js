f24040c942e3832e14370af0412be5bc27e7e670
RaceproxateDev <coxemeftigtipout@gmail.com>
2026-04-21 18:01:29 +0200

    Update save.js

--- Shard Incremental/save.js
+++ Shard Incremental/save.js
@@ -3,9 +3,9 @@ let preventSave = false
 var Data = {
     shards: new OmegaNum(0), shardMult: new OmegaNum(1),
     constructionPoints: new OmegaNum(0), constructionMult: new OmegaNum(1), constructionStorage: new OmegaNum(0),
-    Destructions: new OmegaNum(0), DestructionReq: new OmegaNum(10), DestructionScale: new OmegaNum(2.5), DestructionUnlocked: false,
+    Destructions: new OmegaNum(0), DestructionReq: new OmegaNum(10), DestructionScale: new OmegaNum(2.5),
     ConstructionEnergy: new OmegaNum(0), ConstructionEnergyMult: new OmegaNum(1),
-    RestorationPoints: new OmegaNum(0), RestorationMult: new OmegaNum(1), RestorationStorage: new OmegaNum(0), RestorationUnlocked: false,
+    RestorationPoints: new OmegaNum(0), RestorationMult: new OmegaNum(1), RestorationStorage: new OmegaNum(0),
 
     Buyables: {
         1: {
