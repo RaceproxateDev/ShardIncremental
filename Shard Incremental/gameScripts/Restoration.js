9fa21d3791dc6876a2764f26ae87093cce4e3039
RaceproxateDev <coxemeftigtipout@gmail.com>
2026-05-07 17:41:44 +0200

    a

--- Shard Incremental/gameScripts/Restoration.js
+++ Shard Incremental/gameScripts/Restoration.js
@@ -96,7 +96,7 @@ function CalcRestorationMult() {
     if (Data.Destructions.gte(60)) mult = mult.mul(1.5);
     if (Data.Points.gte(1)) mult = mult.mul(2);
     if (Data.Points.gte(2)) mult = mult.mul(2.5);
-    
+    if (Data.Points.gte(5)) mult = mult.mul(2);
 
     Data.RestorationMult = mult;
     return mult;
